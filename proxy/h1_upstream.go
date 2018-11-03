759b18e29bcf1aecb18f43f0728d4c7e3fd219ab
Phil Kedy <phil.kedy@gmail.com>
2018-11-02 21:50:56 -0400

    Upstream dialing changes

--- proxy/h1_upstream.go
+++ proxy/h1_upstream.go
@@ -5,8 +5,8 @@
 package proxy
 
 import (
-	"crypto/tls"
 	"fmt"
+	"net"
 	"strconv"
 	"strings"
 	"sync"
@@ -33,16 +33,23 @@ type H1Upstream struct {
 	client *fasthttp.Client
 }
 
-func NewH1Upstream(url string, tlsConfig *tls.Config) (Upstream, error) {
+func NewH1Upstream(address string, secure bool, dialer Dialer) (Upstream, error) {
 	client := &fasthttp.Client{
 		MaxIdleConnDuration: 60 * time.Second,
-		TLSConfig:           tlsConfig,
-		DialDualStack:       true, // Support IPV6
+		Dial: func(addr string) (net.Conn, error) {
+			return dialer(addr, secure)
+		},
+		DialDualStack: true, // Support IPV6
+	}
+
+	scheme := "http"
+	if secure {
+		scheme = "https"
 	}
 
 	return &H1Upstream{
-		url:             url,
-		baseURL:         fmt.Sprintf("http://%s", url),
+		url:             address,
+		baseURL:         fmt.Sprintf("%s://%s", scheme, address),
 		currentStreamID: ^uint32(0),
 		client:          client,
 		requests:        make(map[uint32]*fasthttp.Request, 30),
